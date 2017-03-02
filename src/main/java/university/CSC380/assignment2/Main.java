b286e7ac5ea5710098324e99a130656c3c28fdbc
bill <bill@bill-Aspire-E5-571>
2017-03-02 11:14:48 -0500

    Added test methods for polling MTA and Google APIs and getFile

--- src/main/java/university/CSC380/assignment2/Main.java
+++ src/main/java/university/CSC380/assignment2/Main.java
@@ -26,17 +26,17 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException, 
             IOException{
         
-        Utility.startupTasks();
-        
-        HashMap hm = Utility.parseSchedule("Routes.txt");
-        
-        String fileName1 = Utility.getFile("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDVO746CwOhnxOo6KQOrEL1L6as-Ag_sKw&address=Malcom%20x%20bl,%20w%20146%20st", "/home/bill/SchoolWork/csc380/CSC_380_bus_project", "address-data.json");
-        
-        String fileName2 = Utility.getFile("http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=7a22c3e8-61a7-40ff-9d54-714e36f56880", "/home/bill/SchoolWork/csc380/CSC_380_bus_project", "vehicle-monitoring.json");
-        
-        JsonObject j = Utility.parseJson(fileName1);
-        
-        System.out.println("Finished creating JObject");
+//        Utility.startupTasks();
+//        
+//        HashMap hm = Utility.parseSchedule("Routes.txt");
+//        
+//        String fileName1 = Utility.getFile("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDVO746CwOhnxOo6KQOrEL1L6as-Ag_sKw&address=Malcom%20x%20bl,%20w%20146%20st", "/home/bill/SchoolWork/csc380/CSC_380_bus_project", "address-data.json");
+//        
+//        String fileName2 = Utility.getFile("http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=7a22c3e8-61a7-40ff-9d54-714e36f56880", "/home/bill/SchoolWork/csc380/CSC_380_bus_project", "vehicle-monitoring.json");
+//        
+//        JsonObject j = Utility.parseJson(fileName1);
+//        
+//        System.out.println("Finished creating JObject");
         
     }
     
